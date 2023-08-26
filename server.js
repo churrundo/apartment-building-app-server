bdf673d4c6e9e1bd2a952dc0a2c45ff6ed2f942f
churrundo <churrundo@gmail.com>
2023-08-26 16:27:32 -0600

    undo react build

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const path = require('path');
 const app = require("./app");
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
