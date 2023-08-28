da79297a54e67265ebb6eaf01ba1e9c6c2bdeb62
churrundo <churrundo@gmail.com>
2023-08-27 19:47:02 -0600

    log server variables

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ const app = require("./app");
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
+  console.log(process.env.PORT)
+  console.log(process.env.ORIGIN)
   console.log(`Server listening on http://localhost:${PORT}`);
 });
