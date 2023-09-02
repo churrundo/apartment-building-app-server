84e6a859fc0b343641bebafc355ddcea3feddf77
churrundo <churrundo@gmail.com>
2023-09-02 11:29:39 -0600

    refactor poll model

--- models/Poll.model.js
+++ models/Poll.model.js
@@ -1,32 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const pollSchema = new Schema({
+const pollSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
-    votesInFavor: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    votesAgainst: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    status: {
-        type: String,
-        enum: ['Open', 'Closed'],
-        default: 'Open'
-    }
-}, {
-    timestamps: true
-});
+    options: [
+      {
+        text: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        votes: Number,
+        default:0
+      },
+    ],
 
-module.exports = mongoose.model('Poll', pollSchema);
+    voteHashes: [String],
+
+    status: {
+      type: String,
+      enum: ["Open", "Closed"],
+      default: "Open",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Poll", pollSchema);
