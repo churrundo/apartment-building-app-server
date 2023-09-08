59c334272f4f1c171a949605a4b177069ba5e5ca
churrundo <churrundo@gmail.com>
2023-09-07 19:16:51 -0600

    add post and get routes

--- routes/buildings.routes.js
+++ routes/buildings.routes.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const router = express.Router();
-  
-  router.post("/", (req, res, next) => {
-    res.json("Post a new building");
-  });
-  
-  router.get("/:id", (req, res, next) => {
-    res.json(`Retrieve building with ID: ${req.params.id}`);
-  });
-  
-  router.put("/:id", (req, res, next) => {
-    res.json(`Update building with ID: ${req.params.id}`);
-  });
-  
-  router.delete("/:id", (req, res, next) => {
-    res.json(`Delete building with ID: ${req.params.id}`);
-  });
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, address, totalApartments } = req.body;
+
+    // Validation (can be more in-depth, this is basic)
+    if (!name || !address || !totalApartments) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // Check if building already exists based on the address
+    const existingBuilding = await Building.findOne({ address });
+    if (existingBuilding) {
+      return res
+        .status(400)
+        .json({ message: "Building with this address already exists." });
+    }
+
+    // Create a new building
+    const building = new Building({
+      name,
+      address,
+      totalApartments,
+    });
+
+    await building.save();
+    res
+      .status(201)
+      .json({ message: "Building created successfully!", building });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  if (req.query.address) {
+    const building = await Building.findOne({ address: req.query.address });
+    if (!building) {
+      return res.status(404).json({ message: "Building not found." });
+    }
+    return res.json(building);
+  }
+});
+
+router.get("/:buildingId", async (req, res) => {
+  if (req.query.address) {
+  const building = await Building.findById(req.params.buildingId);
+  if (!building) {
+    return res.status(404).json({ message: "Building not found." });
+  }
+  return res.json(building);
+}
+});
+
+router.put("/:id", (req, res, next) => {
+  res.json(`Update building with ID: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res, next) => {
+  res.json(`Delete building with ID: ${req.params.id}`);
+});
 
 module.exports = router;
