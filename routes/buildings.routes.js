ea12601660e732f1e4c735168fa8dbf8924eea34
Juan Pablo <churrundo@gmail.com>
2023-09-10 14:01:41 -0600

    cleaner building routes

--- routes/buildings.routes.js
+++ routes/buildings.routes.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const buildingController = require("../controllers/buildings.controller");
+const {
+  createNewBuilding,
+  getBuildingByAddress,
+  getBuildingById,
+  addUserToBuilding,
+  addPollToBuilding,
+  addAnnouncementToBuilding,
+} = require("../controllers/buildings.controller");
 
-// Create a new building
-router.post("/", buildingController.createNewBuilding);
-
-// Get building by address
-router.get("/", buildingController.getBuildingByAddress);
-
-// Get building by ID
-router.get("/:buildingId", buildingController.getBuildingById);
-
-// Add a user to a building
-router.put("/:buildingId/addUser/:userId", buildingController.addUserToBuilding);
-
-// Add an announcement to a building
-router.put("/:buildingId/addAnnouncement/:announcementId", buildingController.addAnnouncementToBuilding);
-
-// Add a poll to a building
-router.put("/:buildingId/addPoll/:pollId", buildingController.addPollToBuilding);
-
-// Delete a building (Placeholder)
-router.delete("/:id", buildingController.deleteBuilding);
+router.post("/", createNewBuilding);
+router.get("/", getBuildingByAddress);
+router.get("/:buildingId", getBuildingById);
+router.put("/:buildingId/addUser/:userId",addUserToBuilding);
+router.put(  "/:buildingId/addAnnouncement/:announcementId", addAnnouncementToBuilding);
+router.put("/:buildingId/addPoll/:pollId", addPollToBuilding);
 
 module.exports = router;
